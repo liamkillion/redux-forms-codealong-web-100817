abdca31f1a922fe6451d6346f12bb0ac2cf4513e
Liam Killion <liam.killion@gmail.com>
2018-01-09 14:40:11 -0500

    typo

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class CreateTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ text: event.target.value });
+  };
+
   render() {
-    return(
+    return (
       <div>
-        Create Todo Component
+        <form>
+          <p>
+            <label>add todo</label>
+            <input type="text" onChange={event => this.handleChange(event)} />
+          </p>
+          <input type="submit" />
+        </form>
+        <p>{this.state.text}</p>
       </div>
-    )
+    );
   }
 }
 
